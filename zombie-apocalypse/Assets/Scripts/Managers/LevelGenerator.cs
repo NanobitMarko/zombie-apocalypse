0749eca5160260338bbde004f8570751065d0c70
Matej Paradžik <matej.paradzik@nanobit.co>
2016-10-21 17:54:14 +0200

    generateSegment placeholder

--- zombie-apocalypse/Assets/Scripts/Managers/LevelGenerator.cs
+++ zombie-apocalypse/Assets/Scripts/Managers/LevelGenerator.cs
@@ -2,6 +2,23 @@
 using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour {
+
 	[SerializeField]
 	private List<LevelSegment> segments;
+
+	private LevelSegment currentSegment;
+
+
+	public void generateSegment(int difficulty, int humans) {
+
+		Vector2 currentSegmentEnd = currentSegment.EndPosition;
+
+
+		LevelSegment next = segments[(int) Random.Range(0, segments.Count)];
+
+	
+		Vector2 delta = next.StartPosition - currentSegment.EndPosition;
+
+		next.transform.Translate (delta);
+	}
 }
