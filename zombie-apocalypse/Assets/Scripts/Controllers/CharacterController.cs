8e8e3a5cadc28f4b453bcb7ebbaa5874ed15be8d
Marko Vrljičak <marko.vrljicak@nanobit.co>
2016-10-22 00:03:12 +0200

    Jump animation added. Zombie can now jump only once. Removed some unnecessary files.

--- zombie-apocalypse/Assets/Scripts/Controllers/CharacterController.cs
+++ zombie-apocalypse/Assets/Scripts/Controllers/CharacterController.cs
@@ -8,14 +8,26 @@ public class CharacterController : ObjectController {
 
 	[SerializeField]
 	public float movespeed;
+	[SerializeField]
+	protected Animator animator;
+
+	protected bool grounded = true;
+
+	protected bool finishedJumpAnimation = false;
 
 	void Start () {
 		rb.velocity = new Vector2 (movespeed, rb.velocity.y);
 	}
 
-	void OnCollisionEnter2D (Collision2D collision) {
-		if (collision.gameObject.tag == "") {
-			
+	private void OnCollisionEnter2D (Collision2D collision) {
+		if (finishedJumpAnimation && collision.gameObject.tag == "Level Tile") {
+			animator.SetBool ("Grounded", true);
+			grounded = true;
+			finishedJumpAnimation = false;
 		}
+	}
+
+	private void FinishedJumpAnimation () {
+		finishedJumpAnimation = true;
 	}
 }
--- zombie-apocalypse/Assets/Scripts/Controllers/ZombieController.cs
+++ zombie-apocalypse/Assets/Scripts/Controllers/ZombieController.cs
@@ -4,11 +4,19 @@ using UnityEngine.EventSystems;
 
 public class ZombieController : CharacterController {
 
+	public int JumpingPower = 9;
+
+	private int canJump;
+
 	void Update () {
 		rb.velocity = new Vector2 (movespeed, rb.velocity.y);
 	}
 
 	public void Jump (PointerEventData eventData) {
-		rb.AddForce (Vector2.up * 5, ForceMode2D.Impulse);
+		if (grounded) {
+			rb.AddForce (Vector2.up * JumpingPower, ForceMode2D.Impulse);
+			animator.SetBool ("Grounded", false);
+			grounded = false;
+		}
 	}
 }
