2d4d06594df8e3c2bfecff185c0b291f9d8dde88
Lana Lisjak <lanalisjak@Lanas-iMac.local>
2016-10-21 14:15:28 +0200

    added repeating background

--- zombie-apocalypse/Assets/Scripts/CameraFollow.cs
+++ zombie-apocalypse/Assets/Scripts/CameraFollow.cs
@@ -5,18 +5,17 @@ public class CameraFollow : MonoBehaviour {
 
 	GameObject player;
 
-	Transform transform;
+
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
 
-		transform = GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3 (player.transform.position.x, 0, -100);
+		transform.position = new Vector3 (player.transform.position.x, transform.position.y, transform.position.z);
 	}
 
 
