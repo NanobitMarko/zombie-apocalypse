8d70c4f9f79216fda565018d305c5a2cfb0c0612
Matej Paradžik <matej.paradzik@nanobit.co>
2016-10-21 11:57:01 +0200

    kamera prati lika po nešto platforme

---
+++ zombie-apocalypse/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+
+	Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.transform.position.x, 0, -100);
+	}
+
+
+}
---
+++ zombie-apocalypse/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+
+	[SerializeField]
+	public float movespeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2(movespeed, rb.velocity.y);
+	}
+}
