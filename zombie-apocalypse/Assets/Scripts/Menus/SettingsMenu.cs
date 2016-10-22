d0c1616735d92606bbb0311320b8d5a9adf6858f
Iris Veronika Ćelić <iris.veronika.celic@nanobit.co>
2016-10-22 21:54:49 +0200

    new menus ui

---
+++ zombie-apocalypse/Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsMenu : BaseMenu {
+
+	public static SettingsMenu Create () {
+		return Instantiate (Resources.Load <SettingsMenu> ("Prefabs/SettingsMenu"));
+	}
+
+	public void OnMusicClicked () {
+		GameManager.Instance.SoundManager.SetBackgroundMusicActive ();
+	}
+
+	public void OnSoundsClicked () {
+		GameManager.Instance.SoundManager.SetSoundEffectsActive ();
+	}
+
+	public void OnExitClicked () {
+		ExitMenu ();
+	}
+
+	public void ExitMenu () {
+		Destroy (gameObject);
+	}
+
+}
