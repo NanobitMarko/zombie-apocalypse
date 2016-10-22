7ff1f021fa514dedf20ba8e774c9038f55a336a2
Iris Veronika Ćelić <iris.veronika.celic@nanobit.co>
2016-10-22 22:39:52 +0200

    score showing after you die, yay!

--- zombie-apocalypse/Assets/Scripts/Menus/GameOverMenu.cs
+++ zombie-apocalypse/Assets/Scripts/Menus/GameOverMenu.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameOverMenu : BaseMenu {
 
+	public Text Score;
+
 	public static GameOverMenu Create () {
 		return Instantiate (Resources.Load <GameOverMenu> ("Prefabs/GameOverMenu"));
+	}
+
+	public void Start (){
+		Score.text = string.Format("{0:N0}", GameManager.Instance.LevelManager.Zombie.State.Score);
 	}
 
 	public void OnRestartGameClicked () {
